fcb195a9fc02ad0e9570dd7ec74e16ddb0dc947b
madhukar <madhukarchintoju@gmail.com>
2021-08-13 11:37:00 +0530

    feedorg.in used for apis

--- src/Components/apiServices/apiServices.js
+++ src/Components/apiServices/apiServices.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
-const FEED_BE = '13.235.160.1:3000/v1'
+const FEED_BE = 'feedorg.in/v1'
 
 const registerFpcMember = async function (data) {
-  const url = `http://${FEED_BE}/fpomember/register`
+  const url = `https://${FEED_BE}/fpomember/register`
   try {
     const response = await axios.post(url, data)
     return response
@@ -12,7 +12,7 @@ const registerFpcMember = async function (data) {
 }
 
 const loginFpcMember = async function (data) {
-  const url = `http://${FEED_BE}/fpomember/login`
+  const url = `https://${FEED_BE}/fpomember/login`
   try {
     const response = await axios.post(url, data)
     return response
